3a604ef1b317dcc1f78c2054457a8207b9e6a872
Anna Breger, Clemens Karner <clemens.karner@meduniwien.ac.at>
2024-05-28 17:20:20 +0200

    Update haar_psi.py

--- haar_psi.py
+++ haar_psi.py
@@ -1,5 +1,5 @@
 """
-This is a Python Pytorch implementation of the HaarPSI algorithm as presented in "A Haar wavelet-based perceptual similarity index for image quality assessment" by Rafael Reisenhofer, Sebastian Bosse, Gitta Kutyniok and Thomas Wiegand.
+This is a Python PyTorch implementation of the HaarPSI algorithm as presented in "A Haar wavelet-based perceptual similarity index for image quality assessment" by Rafael Reisenhofer, Sebastian Bosse, Gitta Kutyniok and Thomas Wiegand.
 
 Converted by Sören Dittmer and Clemens Karner from the original Python tensorflow implementation written by David Neumann and from the original MATLAB implementation written by Rafael Reisenhofer.
 
