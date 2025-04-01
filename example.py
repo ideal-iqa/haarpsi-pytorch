a43a44594e4fc46396c90fcc0a774682ca36d23e
Anna Breger, Clemens Karner <clemens.karner@meduniwien.ac.at>
2025-04-01 14:22:19 +0200

    Update example.py
    fix typo

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ C=5
 
 #  Choose the parameter alpha in the range [2,8], suggested values:
 #  Natural images: 4.2
-#  Medical images: 5.8
+#  Medical images: 4.9
 Alpha=5.8
 
 (Similarity_score, Local_similarity, Weights)=haarpsi(ImgA,ImgB,C,Alpha)
